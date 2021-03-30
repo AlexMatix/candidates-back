153f15d8765274fba7871ba175ecc00ae8e66f82
Alejandro Rodríguez Serrano <36872513+AlexMatix@users.noreply.github.com>
2021-03-30 14:14:37 -0600

    Passport

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponse;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * A list of the exception types that are not reported.
      *
@@ -40,12 +49,80 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception,$request);
+        }
+
+        if($exception instanceof ModelNotFoundException){
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("No existe ninguna instancia de {$model} con el id especificado",404);
+        }
+
+        if($exception instanceof AuthenticationException){
+            return $this->unauthenticated($request,$exception);
+        }
+
+        if($exception instanceof AuthorizationException){
+            return $this->errorResponse('No posee permisos para ejecutar esta acción',403);
+        }
+
+        if($exception instanceof NotFoundHttpException){
+            return $this->errorResponse('No se encontró la URL especificada',404);
+        }
+
+        if($exception instanceof MethodNotAllowedException){
+            return $this->errorResponse('El método especificado en la petición no es válido',405);
+        }
+
+        if($exception instanceof HttpException){
+            return $this->errorResponse($exception->getMessage(),$exception->getSattusCode());
+        }
+
+        if($exception instanceof  QueryException){
+            $code = $exception->errorInfo[1];
+
+            if($code == 1451){
+                return $this->errorResponse('No se puede elminar de forma permanente el recurso por que esta relacionado con otro',409);
+            }
+        }
+
+        if($exception instanceof InvalidArgumentException){
+            return $this->errorResponse('No autenticado.', 401);
+        }
+
+        if(config('app.debug')){
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Falla inesperada. Intente Despues',500);
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('No autenticado.', 401);
+    }
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors,422);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
+Route::post('oauth/token', 'Auth\LoginController@issueToken');
+//END ROUTES PASSPORT
