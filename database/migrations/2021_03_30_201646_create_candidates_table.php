52ea74f5eb8eb8dbc3ad4f5695099f92e76fc263
AlexMatix <alexrodriguezserrano1595@gmail.com>
2021-04-03 14:59:52 -0600

    Candidate

--- database/migrations/2021_03_30_201646_create_candidates_table.php
+++ database/migrations/2021_03_30_201646_create_candidates_table.php
@@ -15,35 +15,35 @@ class CreateCandidatesTable extends Migration
     {
         Schema::create('candidates', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('father_lastname');
-            $table->string('mother_lastname');
-            $table->string('name');
+            $table->string('father_lastname')->nullable();;
+            $table->string('mother_lastname')->nullable();;
+            $table->string('name')->nullable();;
             $table->string('nickname')->nullable(); //CHICARCAS
-            $table->string('roads'); //AVENIDA  Posible select
-            $table->string('roads_name'); //LOS GIRASOLES
-            $table->integer('outdoor_number'); //2155
+            $table->string('roads')->nullable();; //AVENIDA  Posible select
+            $table->string('roads_name')->nullable();; //LOS GIRASOLES
+            $table->integer('outdoor_number')->nullable();; //2155
             $table->integer('interior_number')->nullable(); //A
-            $table->string('neighborhood'); //LA PAZ
-            $table->string('zipcode'); // 72000
-            $table->string('municipality'); //PUEBLA
-            $table->string('elector_key'); //RYGNHR94012221H700
-            $table->string('ocr'); //876076131459
-            $table->string('cic'); //37078740
-            $table->integer('emission'); //2015
-            $table->string('entity'); //PUEBLA
-            $table->string('section'); //1025
-            $table->date('date_birth'); //15/11/1999
-            $table->string('gender'); // HOMBRE -- MUJER
-            $table->string('birthplace'); //OAXACA
-            $table->integer('residence_time_year'); //2
-            $table->integer('residence_time_month'); //1
-            $table->string('occupation'); //EMPLEADA
-            $table->string('re_election'); //si - no
-            $table->integer('postulate');
-            $table->integer('type_postulate');
-            $table->string('indigenous_group'); //si - no
-            $table->string('group_sexual_diversity'); //si - no
-            $table->string('disabled_group'); //si - no
+            $table->string('neighborhood')->nullable();; //LA PAZ
+            $table->string('zipcode')->nullable();; // 72000
+            $table->string('municipality')->nullable();; //PUEBLA
+            $table->string('elector_key')->nullable();; //RYGNHR94012221H700
+            $table->string('ocr')->nullable();; //876076131459
+            $table->string('cic')->nullable();; //37078740
+            $table->integer('emission')->nullable();; //2015
+            $table->string('entity')->nullable();; //PUEBLA
+            $table->string('section')->nullable();; //1025
+            $table->date('date_birth')->nullable();; //15/11/1999
+            $table->string('gender')->nullable();; // HOMBRE -- MUJER
+            $table->string('birthplace')->nullable();; //OAXACA
+            $table->integer('residence_time_year')->nullable();; //2
+            $table->integer('residence_time_month')->nullable();; //1
+            $table->string('occupation')->nullable();; //EMPLEADA
+            $table->string('re_election')->nullable();; //si - no
+            $table->integer('postulate')->nullable();;
+            $table->integer('type_postulate')->nullable();;
+            $table->string('indigenous_group')->nullable();; //si - no
+            $table->string('group_sexual_diversity')->nullable();; //si - no
+            $table->string('disabled_group')->nullable();; //si - no
             $table->boolean('ine_check')->nullable();
             $table->unsignedBigInteger('postulate_id'); //si - no
             $table->unsignedBigInteger('politic_party_id'); //si - no
