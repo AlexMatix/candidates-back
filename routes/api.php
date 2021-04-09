55e716d0dd330078f1c6614497696c5283171562
Alejandro Rodríguez Serrano <36872513+AlexMatix@users.noreply.github.com>
2021-04-09 14:28:01 -0500

    Report Diputado by district

--- routes/api.php
+++ routes/api.php
@@ -59,5 +59,5 @@ Route::get('getMunicipalities', 'Postulate\PostulateController@getMunicipalities
 
 //REPORTS ROTE
 Route::post('reportDiputadosByDistrictIne', 'CantidateIne\CandidateIneController@reportDiputadosByDistrictIne');
-Route::post('ayuntamientoMunicipalityPoliticParty', 'CantidateIne\ayuntamientoMunicipalityPoliticParty@reportDiputadosByDistrictIne');
+Route::post('ayuntamientoMunicipalityPoliticParty', 'CantidateIne\CandidateIneController@ayuntamientoMunicipalityPoliticParty');
 //END REPORTS ROTE
